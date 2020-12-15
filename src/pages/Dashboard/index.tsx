c0aaea900278414e85e245c4a2702133c58c7762
lopes <lopes@arquia.com>
2020-12-15 11:48:02 -0300

    dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
+import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dasboard: React.FC = () => {
   return (
@@ -14,6 +15,47 @@ const Dasboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/32467674?s=460&u=1a44fec661f7b8bb352c5c1220a7d5462a45870a&v=4"
+            alt="Lucas Fernando"
+          />
+          <div>
+            <strong>Github-Explorer</strong>
+            <p>Github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/32467674?s=460&u=1a44fec661f7b8bb352c5c1220a7d5462a45870a&v=4"
+            alt="Lucas Fernando"
+          />
+          <div>
+            <strong>Github-Explorer</strong>
+            <p>Github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/32467674?s=460&u=1a44fec661f7b8bb352c5c1220a7d5462a45870a&v=4"
+            alt="Lucas Fernando"
+          />
+          <div>
+            <strong>Github-Explorer</strong>
+            <p>Github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
